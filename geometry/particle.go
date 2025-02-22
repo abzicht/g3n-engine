ea5cd822c8d3249b051b7ed030bbe1dc6040db52
Moritz Finke <abzicht@gmail.com>
2025-02-23 00:28:33 +0100

    cleaned up particle geometry

--- geometry/particle.go
+++ geometry/particle.go
@@ -5,10 +5,6 @@
 package geometry
 
 import (
-	"fmt"
-	"math/rand/v2"
-	"unsafe"
-
 	"github.com/g3n/engine/gls"
 	"github.com/g3n/engine/math32"
 )
@@ -17,60 +13,26 @@ type ParticleGeometry struct {
 	Geometry
 	numParticles uint32
 	dimensions   *math32.Vector3
-	// Buffer object of the compute shader that holds the particle positions
-	// as an vec3 array
-	positions gls.BufferObject
 }
-
-//var particlesBufferSize uint32
-//{
-//	var particles_ [30]Particle
-//	numParticles = uint32(len(particles_))
-//	particlesBufferSize = uint32(unsafe.Sizeof(particles_))
-//}
-//callback := func(b_ *gls.BufferRaw, deltaTime time.Duration) {
-//	//b := b_.Typed()
-//	//var particles []Particle = (*[numParticles]Particle)(b_.Address)[:]
-//	var particles []Particle = unsafe.Slice((*Particle)(b_.Address), numParticles)
-//	for i, p := range particles {
-//		fmt.Printf("part. %d (pos: %+v, vel: %+v); ", i, p.pos, p.velocity)
-//	}
-//}
-//particlesBO := gls.NewSSBO(gs, 0,
-//	gls.BO_DYNAMIC_COPY, gls.BO_READ_WRITE, callback,
-//	particlesBufferSize) //.SetInitialBuffer()
-//ssbos := gls.NewBufferObjects()
-//ssbos.Set(particlesBO)
-//p.computeSpecs = gls.NewComputeSpecs(computeProg, "4_3", *gls.NewShaderDefines(), ssbos)
 
 // NewParticles creates a particle geometry with the specified number of
-// particles whose position is controlled by the provided buffer that is
+// particles whose position is controlled by a buffer that is
 // expected to be an array of vec3s with length numParticles and that binds to
-// binding index 0. If
-// positions==nil, a random distribution of particles is applied (you should
-// really provide a custom buffer to achieve meaningful effects).
-func NewParticles(numParticles uint32, positions gls.BufferObject, dimensions *math32.Vector3) *ParticleGeometry {
+// binding index 0.
+func NewParticles(numParticles uint32, dimensions *math32.Vector3) *ParticleGeometry {
 	p := new(ParticleGeometry)
-	p.Init(numParticles, positions, dimensions)
+	p.Init(numParticles, dimensions)
 	return p
 }
-func (p *ParticleGeometry) Init(numParticles uint32, positions gls.BufferObject, dimensions *math32.Vector3) {
+func (p *ParticleGeometry) Init(numParticles uint32, dimensions *math32.Vector3) {
 	p.Geometry.Init()
 	p.SetBoundingBox(dimensions)
 	p.numParticles = numParticles
 	p.dimensions = dimensions
-	// Create buffers
-	p.positions = positions
-	//.AddAtrib(gls.VertexColor)
 }
 
 func (p *ParticleGeometry) Items() int {
 	return int(p.numParticles)
-}
-func (g *ParticleGeometry) Indices() math32.ArrayU32 {
-
-	fmt.Println("Helloo")
-	return g.indices
 }
 
 // Set the bounding box dimensions for the particle geometry
@@ -114,18 +76,6 @@ func (p *ParticleGeometry) RenderSetup(gs *gls.GLS) {
 		p.handleVAO = gs.GenVertexArray()
 		// Save pointer to gs indicating initialization was done
 		p.gs = gs
-		// User didn't set a buffer, let's prepare a random one
-		if p.positions == nil {
-			particles := math32.NewArrayF32(int(p.numParticles), int(p.numParticles))
-			for i := 0; i < len(particles); i += 3 {
-				particles[i] = (rand.Float32() - 0.5) * p.dimensions.X / 2
-				particles[i+1] = (rand.Float32() - 0.5) * p.dimensions.Y / 2
-				particles[i+2] = (rand.Float32() - 0.5) * p.dimensions.Z / 2
-			}
-			ssbo := gls.NewSSBO(p.gs, 0, gls.DYNAMIC_COPY, gls.BO_READ_WRITE, nil, uint32(unsafe.Sizeof(&particles)))
-			ssbo.SetInitialBuffer(gls.NewBufferRaw(unsafe.Pointer(unsafe.SliceData(particles)), uint32(unsafe.Sizeof(&particles))))
-			p.positions = ssbo
-		}
 	}
 
 	// Update VBOs
@@ -133,10 +83,4 @@ func (p *ParticleGeometry) RenderSetup(gs *gls.GLS) {
 	for _, vbo := range p.vbos {
 		vbo.Transfer(gs)
 	}
-	//gs.DrawArrays(gls.POINTS, 0, int32(p.numParticles))
-}
-
-// Set the positions buffer for the particles in this geometry
-func (p *ParticleGeometry) SetPositionsBO(positions gls.BufferObject) {
-	p.positions = positions
 }
